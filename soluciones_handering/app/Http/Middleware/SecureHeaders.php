df223ecb9dc604e650b9a58cfb6a1e4ac928375c
Erick Solorzano <ericksolorzanolopez@gmail.com>
2026-03-04 02:04:32 -0600

    Agregar permission policy

---
+++ soluciones_handering/app/Http/Middleware/SecureHeaders.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecureHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        $response->headers->set(
+            'Permissions-Policy',
+            'geolocation=(), camera=(), microphone=(), payment=(), fullscreen=(self)'
+        );
+
+        return $response;
+    }
+}
--- soluciones_handering/routes/web.php
+++ soluciones_handering/routes/web.php
@@ -1,7 +1,71 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response; 
+use App\Http\Middleware\SecureHeaders;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+
+Route::get('/hardening', function (Request $request) {
+
+    // ✅ Cookie protegida
+    $cookie = cookie(
+        'session_id',
+        'SESSION123456',
+        60,        // minutos
+        '/',
+        null,
+        false,     // secure (true en HTTPS)
+        true,      // ✅ HttpOnly
+        false,
+        'Strict'
+    );
+
+    return response()
+        ->view('hardening', [
+            'nombre' => $request->query('nombre')
+        ])
+        ->cookie($cookie);
+});
+
+Route::get('/http-only', function () {
+
+    $cookie = cookie(
+        'session_id',
+        'SESSION123456',
+        60,
+        '/',
+        null,
+        false,
+        true,
+        false,
+        'Strict'
+    );
+
+    return response()
+        ->view('http_only')
+        ->cookie($cookie);
+});
+
+
+Route::get('/permission-policy', function (Request $request) {
+
+    // Generar la vista como Response
+    $response = response()->view('permission_policy');
+
+    // Agregar la cabecera segura
+    $response->headers->set(
+        'Permissions-Policy',
+        'geolocation=(), camera=(), microphone=(), payment=(), fullscreen=(self)'
+    );
+
+    return $response;
 });
