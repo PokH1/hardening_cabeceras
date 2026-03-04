eab6f9f760a9847853e6ef945feaca3b0c5379e5
Erick Solorzano <ericksolorzanolopez@gmail.com>
2026-03-04 01:09:31 -0600

    Agregar vistas sobre http only

--- soluciones_handering/routes/web.php
+++ soluciones_handering/routes/web.php
@@ -1,7 +1,52 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::get('/hardening', function (Request $request) {
+
+    // ✅ Cookie protegida
+    $cookie = cookie(
+        'session_id',
+        'SESSION123456',
+        60,        // minutos
+        '/',
+        null,
+        false,     // secure (true en HTTPS)
+        true,      // ✅ HttpOnly
+        false,
+        'Strict'
+    );
+
+    return response()
+        ->view('hardening', [
+            'nombre' => $request->query('nombre')
+        ])
+        ->cookie($cookie);
+});
+
+Route::get('/http-only', function () {
+
+    $cookie = cookie(
+        'session_id',
+        'SESSION123456',
+        60,
+        '/',
+        null,
+        false,
+        true,
+        false,
+        'Strict'
+    );
+
+    return response()
+        ->view('http_only')
+        ->cookie($cookie);
 });
